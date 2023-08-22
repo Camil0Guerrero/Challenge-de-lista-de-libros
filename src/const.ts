62f239a38b1bd848c2dcdb7b3b711d264855c870
Camil0Guerrero <guerrerocontrerascristiancamil@gmail.com>
2023-08-22 11:41:24 -0500

    Updates

--- src/const.ts
+++
@@ -1,7 +0,0 @@
-export const GENRE_LIST = {
-	All: 'All',
-	Fantasy: 'Fantasía',
-	SciFi: 'Ciencia ficción',
-	Zombies: 'Zombies',
-	Terror: 'Terror',
-}
--- src/utils/functions/getBooks.ts
+++
@@ -1,6 +0,0 @@
-import { Books } from '../../types'
-import json from '../../books.json'
-
-export async function getBooks(): Promise<Books> {
-	return json
-}
